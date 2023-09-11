0a6710871265b45a0a7f76ef6578f4af5c7f5c74
Reva Yoga Pradana <reva.yoga@grabtaxi.com>
2023-09-11 22:39:42 +0700

    (update) [KASIR-6] Can edit previous order for same menu

--- src/order_center.java
+++ src/order_center.java
@@ -16,29 +16,29 @@ abstract class order_center {
     static List<List<Integer>> order = new ArrayList<>(); //untuk menyimpan id menu & jumlah pesanan
     static List<String> description = new ArrayList<>(); //untuk menyimpan deskripsi dari pesanan
     
-    public static void setValue(int index, int count, String desc, boolean isNewOrder) { //method untuk menambah & Perbarui pesanan
-        if(isNewOrder) {
-            Integer[] temp = {index,count}; // id menu & jumlah pesanan dimaukan kedalam arraylist
+    public static void setValue(int indexOrder, int indexMenu, int count, String desc) { //method untuk menambah & Perbarui pesanan
+        if(indexOrder >= order.size()) { // new order
+            Integer[] temp = {indexMenu,count}; // id menu & jumlah pesanan dimaukan kedalam arraylist
             order.add(new ArrayList(Arrays.asList(temp))); // memasukan arraylist menu yang dipesan ke arraylist order
             description.add(desc); //memasukan deskripsi pesanan
         } else {
             if(count <= 0) { // jika jumlah yang dipesan adalah 0, maka menu tersebut akan di hapus dari pesanan
-                order.remove(index);
-                description.remove(index);
+                order.remove(indexOrder);
+                description.remove(indexOrder);
             } else { // jika jumlah pesanan bukan 0, akan memperbarui pesanan yang sudah ada
-                order.get(index).set(1, count); // perbarui id menu
-                description.set(index, desc); // perbarui deskripsi
+                order.get(indexOrder).set(1, count); // perbarui id menu
+                description.set(indexOrder, desc); // perbarui deskripsi
             }
         }
     }
-    public static int setDisplay(int index){ // methode untuk memeriksa menu yang dipilih sudah dipesan atau belum.
-        int cek = -1;
+    public static List<Integer> setDisplay(int index) { // metode untuk memeriksa menu yang dipilih sudah dipesan atau belum.
+        List<Integer> indexOrder = new ArrayList<>();
         for (int i = 0; i < order.size(); i++) {
             if(index == order.get(i).get(0)){
-                cek = i;
+                indexOrder.add(i);
             }
         }
-        return cek;
+        return indexOrder;
     }
     
     public static void clearOrder(){ //methode untuk menghapus semua isi arraylist order & description
